0d259d08096339cb061efa873652795cfa16e0f3
Rob Brunett <rb3@Robs-Air.lan>
2021-04-16 16:49:25 -0400

    debugged game and added CSS

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ var compOptions = ['R', 'P', 'S'];
 
 var game = function(){
     //Prompts player to enter their choice and sets variable for use in remainder of function
-    var userChoice = prompt('Enter R, P, or S');
+    var userChoice = window.prompt('Enter R, P, or S');
     //Converts player choice to upper case letter for cleaner compare to computer's choice
     userChoice = userChoice.toUpperCase();
     //Function which enables a random selection of R,P,or S from the compOptions global variable
     var choices = Math.floor(Math.random() * compOptions.length);
     //Variable that assigns the random choice generated in choices variable above to the compChoice variable
-    var compChoice = options[choices];
+    var compChoice = compOptions[choices];
     //If statement used to alert player of a tie and add 1 to the var ties for record keeping
     if (userChoice === compChoice){
         ties++;
@@ -31,13 +31,11 @@ var game = function(){
     }
     //If conditions above are not met this statement will assume the player lost the game
     else {
-        loses++;
+        losses++;
         alert('Loser!');
     }
     //Alert to be shown between games to show the running tally of wins, losses, and ties during the current session
-    alert(
-        "Totals:\n Wins: " + wins "\nLosses: " + losses + "\nTies: " + ties
-    );
+    alert("Totals:\nWins: " + wins + "\nLosses: " + losses + "\nTies: " + ties);
     //Variable that creates a confirmation pop-up to ask if the player would like to play another round
     var nextRound = confirm("Would you like to play again?")
     //If player chooses yes in the confirmation pop-up the function starts again
